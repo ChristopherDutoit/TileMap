ac37da9c39380635958277922df522efc4ae4d7d
Félix Viart <viart.felix2017@gmail.com>
2022-01-17 16:13:55 +0100

    Première version du système de cliker

--- app/src/main/java/fr/iutlens/dubois/carte/ClickerActivity.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/ClickerActivity.kt
@@ -2,10 +2,24 @@ package fr.iutlens.dubois.carte
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class ClickerActivity : AppCompatActivity() {
+    private var clc: Int=0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clicker)
+
+        val adder: Button = findViewById(R.id.bout)
+        val txt: TextView = findViewById(R.id.vueTexte)
+
+        adder.setOnClickListener {
+            clc++
+        }
+
+        txt.text= clc.toString()
     }
+
 }
