7f84a9aa91ae7798981fa90fa46d14a407b24b99
erwan <bmaxpro@gmail.com>
2022-01-17 17:13:39 +0100

    Modification des fichiers

--- app/src/main/java/fr/iutlens/dubois/carte/PuzzleActivity.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/PuzzleActivity.kt
@@ -4,7 +4,6 @@ import android.graphics.Matrix
 import android.os.Bundle
 import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
-import fr.iutlens.dubois.carte.Decor.Companion.circ
 import fr.iutlens.dubois.carte.sprite.BasicSprite
 import fr.iutlens.dubois.carte.sprite.SpriteList
 import fr.iutlens.dubois.carte.sprite.TiledArea
@@ -14,7 +13,7 @@ import fr.iutlens.dubois.carte.utils.SpriteSheet
 
 class PuzzleActivity : AppCompatActivity() {
 
-    private val circ by lazy { TiledArea(R.drawable.circuit, Decor(Decor.circ)) }
+    private val cir by lazy { TiledArea(R.drawable.circuit, Decor(Decor.circ)) }
     private val gameView by lazy { findViewById<GameView>(R.id.gameView) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,22 +22,24 @@ class PuzzleActivity : AppCompatActivity() {
 
         SpriteSheet.register(R.drawable.circuit, 5, 4, this)
 
-        configDrag()
+        //configDrag()
     }
+
     private fun configDrag() {
         // Création des différents éléments à afficher dans la vue
         val list = SpriteList() // Notre liste de sprites
         for(i in 1..7){ // On crée plusieurs sprites aléatoires
-            list.add(BasicSprite(R.drawable.circuit, circ,
-                (circ.data.sizeX*Math.random()).toFloat(),
-                (circ.data.sizeY*Math.random()).toFloat()))
+            list.add(BasicSprite(R.drawable.circuit, cir,
+                (cir.data.sizeX*Math.random()).toFloat(),
+                (cir.data.sizeY*Math.random()).toFloat(),
+                (0..2).random()))
         }
 
         // Configuration de gameView
         gameView.apply {
-            background = circ
+            background = cir
             sprite = list
-            transform = FitTransform(this, circ, Matrix.ScaleToFit.CENTER)
+            transform = FitTransform(this, cir, Matrix.ScaleToFit.CENTER)
         }
         gameView.onTouch = this::onTouchDrag
         gameView.invalidate() // On demande à rafraîchir la vue
