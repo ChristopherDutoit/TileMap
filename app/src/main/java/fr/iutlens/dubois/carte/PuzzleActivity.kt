ee4e3aafefa333edf48e1ea997305cc43e00e57e
erwan <bmaxpro@gmail.com>
2022-01-17 14:56:16 +0100

    Modification du fichier PuzzleActivity.kt et ajout image dans le dossier drawable

--- app/src/main/java/fr/iutlens/dubois/carte/PuzzleActivity.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/PuzzleActivity.kt
@@ -1,11 +1,13 @@
 package fr.iutlens.dubois.carte
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class PuzzleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_puzzle)
+
     }
 }
