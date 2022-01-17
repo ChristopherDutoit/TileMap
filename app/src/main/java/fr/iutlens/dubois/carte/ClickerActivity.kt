7bb71faa0ed3f382a0aa889789e1c3f6eb8f70a3
Félix Viart <viart.felix2017@gmail.com>
2022-01-17 16:39:01 +0100

    Clicker qui marche (ajout, affichage dans la vue et maximum).

--- app/src/main/java/fr/iutlens/dubois/carte/ClickerActivity.kt
+++ app/src/main/java/fr/iutlens/dubois/carte/ClickerActivity.kt
@@ -7,19 +7,26 @@ import android.widget.TextView
 
 class ClickerActivity : AppCompatActivity() {
     private var clc: Int=0
+    private var max: Int=20
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clicker)
 
         val adder: Button = findViewById(R.id.bout)
-        val txt: TextView = findViewById(R.id.vueTexte)
 
         adder.setOnClickListener {
             clc++
+            val res: TextView = findViewById(R.id.vueTexte)
+
+            if (clc <= max) {
+                res.text=clc.toString()
+            } else {
+                res.text="Tu as atteint le maximum ("+max.toString()+")"
+            }
         }
 
-        txt.text= clc.toString()
+        //txt.text=clc.toString()
     }
 
 }
