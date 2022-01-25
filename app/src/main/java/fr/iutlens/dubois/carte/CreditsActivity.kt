024b869971de78c031c74bb69b95774465a34d9b
Debruyne <lucas0000.milow@gmail.com>
2022-01-25 10:42:38 +0100

    Ajouts du bouton, de la class et de la vue credits

---
+++ app/src/main/java/fr/iutlens/dubois/carte/CreditsActivity.kt
@@ -0,0 +1,11 @@
+package fr.iutlens.dubois.carte
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreditsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_credits)
+    }
+}
